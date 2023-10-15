da2d0da8c9ed5436bf7a6c51416b9e3d74704955
Erd4 <alexander.kekkas@gmail.com>
2023-10-15 16:20:36 +0800

    rearranged regions, corrected some off locations

--- mapkl.py
+++ mapkl.py
@@ -16,20 +16,20 @@ m = folium.Map(location=[3.1390, 101.6869], zoom_start=11)
 
 # Your list of latitudes and longitudes
 data = {'The Flow Studio': [
-    (3.1277706972688635, 101.67439875621255),
+    (3.1274188,101.6745625),
     (3.1519383363602462, 101.66553696882904),
     (3.138249, 101.63008),
     (3.052161, 101.671278)
     ],
     'karma yoga': [
-        (3.1323484914363386, 101.67129709581177)
+        (3.1318964,101.6711135)
         ],
     'Life Hot Yoga': [
         (3.149766037831857, 101.59387235348427),
         (3.167091133373805, 101.65190068232039)
         ],
     'Celebrity Fitness': [
-        (3.130551466205832, 101.67138542279474),
+        (3.1301827,101.6710671),
         (3.1512698118957534, 101.6169901603),
         (3.0698452683269846, 101.69535177114773),
         (3.1193243119004923, 101.67848784897004),
@@ -90,65 +90,70 @@ data = {'The Flow Studio': [
     }
 
 regions = {'Bangsar': [
-    (3.1277706972688635, 101.67439875621255),
-    (3.1323484914363386, 101.67129709581177),
-    (3.130551466205832, 101.67138542279474),
+    (3.1274188,101.6745625),
+    (3.1318964,101.6711135),
+    (3.1301827,101.6710671),
     (3.1384434073561533, 101.6688006580661)
     ],
-    'Damansara Heights': [
-        (3.1519383363602462, 101.66553696882904),
+    'Kota Damansara': [
         (3.149766037831857, 101.59387235348427),
-        (3.1512698118957534, 101.6169901603),
-        (3.1300817056560275, 101.59657197017448),
-        (3.12873404090602, 101.63503775348423),
+        (3.1212672,101.5954205),
         (3.1550800734689615, 101.59488056882898)],
     'Bukit Bintang': [
         (3.153035935376106, 101.7097521093033),
+        (3.1285862842180845, 101.71357731573829),
         (3.1662218740184365, 101.71750229581187),
         (3.1408348512615536, 101.70754993813944),
         (3.16200592618416, 101.74212885991994)
         ],
     'Bukit Jalil': [
         (3.052161, 101.671278),
-        (3.0698452683269846, 101.69535177114773),
         (3.052142720526545, 101.67918610039212),
-        (3.078323903350539, 101.69372597375367),
         (3.0543866096330508, 101.67000526697564)
         ],
     'Mont Kiara': [
+        (3.1519383363602462, 101.66553696882904),
         (3.167091133373805, 101.65190068232039),
         (3.1682057077947823, 101.65239181115658),
         (3.171403844928022, 101.66593865348423)
         ],
     'Mid-Valley': [
         (3.1193243119004923, 101.67848784897004),
-        (3.0927007030010296, 101.68761344990328),
-        (3.1285862842180845, 101.71357731573829),
         (3.1244613625431485, 101.67427181573822)
         ],
     'Kepong Village': [
         (3.1958333217962744, 101.63381297508522)
         ],
-    'Buchong Jaya': [
-        (3.0457372128617792, 101.62267624933123)
+    'Puchong Jaya': [
+        (3.0448612,101.622157),
         ],
     'Kota Kemuning': [
         (3.010448265835054, 101.53854430327854)
         ],
     'Cheras': [
         (3.0978648832843207, 101.75346413257392),
-        (3.036385337758758, 101.76508320930336),
         (3.0531466874387894, 101.73909438046711),
-        (3.095408586930257, 101.73852216697564)],
+        (3.036385337758758, 101.76508320930336),
+        (3.095408586930257, 101.73852216697564)
+        ],
     'TTDI': [
-        (3.138249, 101.63008)
+        (3.138249, 101.63008),
+        (3.1512698118957534, 101.6169901603),
+        (3.12873404090602, 101.63503775348423),
         ],
     'Seri Kembandang': [
         (3.017641745178442, 101.69612923218372)
         ],
-    'Jalan Ipoh': [
+    'Sentul': [
         (3.179423396637816, 101.68323264272207)
-        ]
+        ],
+    'Sri Petalang': [
+        (3.0698452683269846, 101.69535177114773),
+        (3.078323903350539, 101.69372597375367)
+    ],
+    'Kuchai': [
+        (3.0927007030010296, 101.68761344990328)
+    ]
     }
 
 #Function to find the region of a studio
@@ -165,77 +170,6 @@ def find_studios(lat, lon, data):
         if (lat, lon) in coords:
             matching_studios.append(studio)
     return matching_studios
-
-# Radio button for Studios vs. Regions
-selection_type = st.radio("Select by:", ["Studios", "Regions"])
-
-# Initialize a dictionary to store region-wise studio lists
-region_studio_mapping = {region: [] for region in regions}
-
-# Update the mapping based on data
-for studio, coords in data.items():
-    for coord in coords:
-        region = find_region(coord[0], coord[1], regions)
-        if region:
-            region_studio_mapping[region].append(studio)
-
-if selection_type == "Studios":
-    # Implementing the "Select All" checkbox for studios
-    select_all = st.checkbox("Select all Studios")
-    if select_all:
-        selections = list(data.keys())
-    else:
-        selections = st.multiselect("Which Yoga Studio do you want to see?", list(data.keys()))
-
-    # Combine data from all selected studios into a single list and add markers with popups
-    combined_data = []
-    for s in selections:
-        combined_data.extend(data[s])
-        for point in data[s]:
-            region = find_region(point[0], point[1], regions)
-            popup_text = f"<b>{s}</b><br><br><i>Region: {region}</i>"
-            folium.CircleMarker(
-                location=point,
-                radius=6,
-                color="rgba(255, 255, 255, 0)",
-                fill=True,
-                fill_color="rgba(255, 255, 255, 0)",
-                fill_opacity=0,
-                popup=popup_text
-            ).add_to(m)
-
-else:
-    # Implementing the "Select All" checkbox for regions
-    select_all = st.checkbox("Select all Regions")
-    if select_all:
-        selections = list(regions.keys())
-    else:
-        selections = st.multiselect("Which Region do you want to see?", list(regions.keys()))
-
-    # Combine data from all selected regions into a single list and add markers with popups
-    combined_data = []
-    for r in selections:
-        combined_data.extend(regions[r])
-        for point in regions[r]:
-            studios = find_studios(point[0], point[1], data)
-            studios_text = ', '.join(studios)
-            popup_text = f"<b>{studios_text}</b><br><br><i>Region: {r}</i>"
-            folium.CircleMarker(
-                location=point,
-                radius=6,
-                color="rgba(255, 255, 255, 0)",
-                fill=True,
-                fill_color="rgba(255, 255, 255, 0)",
-                fill_opacity=0,
-                popup=popup_text
-            ).add_to(m)
-
-
-# Add a single heatmap to the base map using the combined data
-HeatMap(combined_data).add_to(m)
-
-# Save the map to an HTML file
-st_folium(m,height=600,  width=700)
 
 # Function to format regions and studios in columns
 def format_columns(regions_studios):
@@ -271,8 +205,95 @@ def format_columns(regions_studios):
     table_html += "</tr></table>"
     return table_html
 
+def create_streetview(lat,lon):
+    return f"http://maps.google.com/maps?q=&layer=c&cbll={lat},{lon}&cbp=12,0,0,0,0)"
 
 
+# Radio button for Studios vs. Regions
+selection_type = st.radio("Select by:", ["Studios", "Regions"])
+
+# Initialize a dictionary to store region-wise studio lists
+region_studio_mapping = {region: [] for region in regions}
+
+# Update the mapping based on data
+for studio, coords in data.items():
+    for coord in coords:
+        region = find_region(coord[0], coord[1], regions)
+        if region:
+            region_studio_mapping[region].append(studio)
+
+if selection_type == "Studios":
+    # Implementing the "Select All" checkbox for studios
+    select_all = st.checkbox("Select all Studios")
+    if select_all:
+        selections = list(data.keys())
+    else:
+        selections = st.multiselect("Which Yoga Studio do you want to see?", list(data.keys()))
+
+    # Combine data from all selected studios into a single list and add markers with popups
+    combined_data = []
+    for s in selections:
+        combined_data.extend(data[s])
+        for point in data[s]:
+            region = find_region(point[0], point[1], regions)
+            streetview_url = create_streetview(point[0], point[1])
+            popup_text = f"""
+            <div style="width: 200px;">
+                <b>{s}</b><br><br>
+                <i>Region: {region}</i><br><br>
+                <a href='{streetview_url}' target='_blank'>Open in Street View</a>
+            </div>
+            """
+            folium.CircleMarker(
+                location=point,
+                radius=6,
+                color="rgba(255, 255, 255, 0)",
+                fill=True,
+                fill_color="rgba(255, 255, 255, 0)",
+                fill_opacity=0,
+                popup=popup_text
+            ).add_to(m)
+
+else:
+    # Implementing the "Select All" checkbox for regions
+    select_all = st.checkbox("Select all Regions")
+    if select_all:
+        selections = list(regions.keys())
+    else:
+        selections = st.multiselect("Which Region do you want to see?", list(regions.keys()))
+
+    # Combine data from all selected regions into a single list and add markers with popups
+    combined_data = []
+    for r in selections:
+        combined_data.extend(regions[r])
+        for point in regions[r]:
+            studios = find_studios(point[0], point[1], data)
+            studios_text = ', '.join(studios)
+            streetview_url = create_streetview(point[0], point[1])
+            popup_text = f"""
+            <div style="width: 200px;">
+                <b>{studios_text}</b><br><br>
+                <i>Region: {r}</i><br><br>
+                <a href='{streetview_url}' target='_blank'>Open in Street View</a>
+            </div>
+            """
+            folium.CircleMarker(
+                location=point,
+                radius=6,
+                color="rgba(255, 255, 255, 0)",
+                fill=True,
+                fill_color="rgba(255, 255, 255, 0)",
+                fill_opacity=0,
+                popup=popup_text
+            ).add_to(m)
+
+
+
+# Add a single heatmap to the base map using the combined data
+HeatMap(combined_data).add_to(m)
+
+# Save the map to an HTML file
+st_folium(m,height=600,  width=700)
 
 if selection_type == "Studios":
     regions_studios = {}
